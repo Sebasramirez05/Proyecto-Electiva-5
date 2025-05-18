0135c193a939164dee3fafa273b6a650d8687052
Sebas <sebasuni05@gmail.com>
2025-05-18 14:04:23 -0400

    prota

--- game.js
+++ game.js
@@ -7,11 +7,17 @@ export class Game extends Phaser.Scene {
     preload() {
       this.load.image('background', 'images/background.png');
       this.load.image('gameover', 'images/gameover.png');
+      this.load.spritesheet('player', 'images/Larry.png', {
+        frameWidth: 20,
+        frameHeight:23
+      });
     }
   
     create() {
       this.add.image(400, 250, 'background').setDisplaySize(800, 500);
       this.gameoverImage = this.add.image(400, 90, 'gameover');
+      this.player = this.physics.add.sprite(400, 500, 'player');
+      this.player.setCollideWorldBounds(true);
     }
   
   }
