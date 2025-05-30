76556bc0a10ed23e92f255e32ecc95aa6293ec27
unknown <pinkyker005gmail.com>
2025-05-30 02:07:29 -0400

    iglu terminado

--- game.js
+++ game.js
@@ -45,12 +45,9 @@ export class Game extends Phaser.Scene{
   }
 
   create() {
-<<<<<<< HEAD
 
     
-=======
     this.scene.start('nivel2');
->>>>>>> 633aeada10665af55083b1665e8703ac57e801ff
     this.add.image(0, 150, 'agua').setOrigin(0, 0).setScale(1.1, 1).setDepth(0);
     this.add.image(0, -180, 'arboles').setOrigin(0, 0).setScale(0.42).setDepth(1);
  
@@ -135,14 +132,11 @@ crearPajaro(850, filas[2].y - 50, -75);
 // Pájaro 4 (izquierda → derecha)
 crearPajaro(-150, filas[3].y - 50, 75);
 
-<<<<<<< HEAD
 
 
 
     crearPajaro(850, 120, -100);
     crearPajaro(-50, 200, 100);
-=======
->>>>>>> 633aeada10665af55083b1665e8703ac57e801ff
 
 
     const permitirAtravesar = (jugador, plataforma) => {
@@ -209,18 +203,7 @@ crearPajaro(-150, filas[3].y - 50, 75);
     this.iglúCompleto = true;
   }
 }
-
-<<<<<<< HEAD
-=======
-      if (this.bloques === this.maxBloques) {
-        console.log("¡Iglú completo! Nivel terminado.");
-        this.time.delayedCall(1000, () => {
-          this.scene.start('nivel2'); 
-        });
-      };
-    }
->>>>>>> 633aeada10665af55083b1665e8703ac57e801ff
-  }
+}
 
   update() {
     // Mover al jugador con la plataforma si está parado sobre ella
@@ -277,10 +260,7 @@ crearPajaro(-150, filas[3].y - 50, 75);
         plataforma.x = -plataforma.displayWidth / 2;
       }
     });
-<<<<<<< HEAD
  
-=======
->>>>>>> 633aeada10665af55083b1665e8703ac57e801ff
 
     this.pajaros.children.iterate(pajaro => {
       if (pajaro.x < -100 && pajaro.body.velocity.x < 0) {
@@ -290,19 +270,15 @@ crearPajaro(-150, filas[3].y - 50, 75);
       }
     });
 
-<<<<<<< HEAD
     //pasar de nivel si el iglú está completo y el jugador está sobre la puerta
-  if (this.iglúCompleto && this.puerta) {
-    const distancia = Phaser.Math.Distance.Between(
+    if (this.iglúCompleto && this.puerta) {
+      const distancia = Phaser.Math.Distance.Between(
       this.jugador.x, this.jugador.y,
       this.puerta.x, this.puerta.y
-    );
-    if (distancia < 40) {
+      );
+      if (distancia < 40) {
       this.scene.start('nivel2');
+      }
     }
-  }
-
-=======
->>>>>>> 633aeada10665af55083b1665e8703ac57e801ff
   }
 }
